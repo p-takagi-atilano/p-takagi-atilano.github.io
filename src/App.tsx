1e4393c282aced50bcad3ca87fda897b11c8259e
Paolo Takagi-Atilano <paolotakagi-atilano@Paolos-MacBook-Air.local>
2023-09-12 10:55:08 -0600

    new with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen bg-paolo-bg-color">
+      <div className="text-4xl font-extrabold text-paolo-color">
+        Paolo Takagi-Atilano
+      </div>
+      <div style={{ flexGrow: 1 }}>asdf</div>
+    </div>
+  );
+}
+
+export default App;
