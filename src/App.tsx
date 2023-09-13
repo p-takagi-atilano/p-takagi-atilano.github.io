cb9caae88e843529fe6919264620026238fadf92
thetroublesomepickle <canopy003@canopy003s-MacBook-Pro.local>
2023-09-13 15:10:10 -0600

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="h-screen bg-paolo-bg-color">
-      <div className="text-4xl font-extrabold text-paolo-color">
-        Paolo Takagi-Atilano
+    <div className="h-full bg-paolo-bg-color p-5">
+      <div className="p-3 border-2 border-paolo-color rounded-3xl h-full">
+        <div className="flex flex-row justify-center">
+          <div className="text-4xl font-extrabold text-paolo-color">
+            Paolo Takagi-Atilano
+          </div>
+        </div>
+        <hr className="h-1 border-0 bg-paolo-color" />
       </div>
-      <div style={{ flexGrow: 1 }}>asdf</div>
     </div>
   );
 }
